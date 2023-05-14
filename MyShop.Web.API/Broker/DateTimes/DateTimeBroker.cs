c6a86b6c4e01f83a9d822d2f31b2318515672073
Sandun Sulakshana <sandung@cbctechsol.com>
2023-05-14 13:11:35 +0530

    Added broker for logging and datetime

---
+++ MyShop.Web.API/Broker/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,7 @@
+﻿namespace MyShop.Web.API.Broker.DateTimes
+{
+    public class DateTimeBroker : IDateTimeBroker
+    {
+        public DateTimeOffset GetCurrentDateTime() => DateTime.UtcNow;
+    }
+}
---
+++ MyShop.Web.API/Broker/DateTimes/IDateTimeBroker.cs
@@ -0,0 +1,7 @@
+﻿namespace MyShop.Web.API.Broker.DateTimes
+{
+    public interface IDateTimeBroker
+    {
+        DateTimeOffset GetCurrentDateTime();
+    }
+}
